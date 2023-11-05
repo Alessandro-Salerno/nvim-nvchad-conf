2982ce1d7e69819577ce2ef393dc163a956391eb
Alessandro-Salerno <OxyGen05@outlook.it>
2023-11-05 16:09:12 +0100

    php maybe?

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -7,6 +7,7 @@ local lspconfig = require("lspconfig")
 lspconfig.texlab.setup{}
 lspconfig.zls.setup{}
 lspconfig.gopls.setup{}
+lspconfig.phpactor.setup{}
 lspconfig.pyright.setup{
   on_attach = on_attach,
   settings = {
